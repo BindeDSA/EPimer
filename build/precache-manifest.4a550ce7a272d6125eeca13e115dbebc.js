786cbbb32f743cbc062b6418a6704ef8dfac6ead
Jabari Nuruddin <jabarinuruddin@gmail.com>
2020-05-24 19:38:14 -0400

    added build

---
+++ build/precache-manifest.4a550ce7a272d6125eeca13e115dbebc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8dee61a0b36eed8ce7a39f9f52ac8ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeb102a72ac5ec584295",
+    "url": "/static/css/2.a2c18f5e.chunk.css"
+  },
+  {
+    "revision": "aeb102a72ac5ec584295",
+    "url": "/static/js/2.3c62673e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3c62673e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae7291b33dfc5bc3d061",
+    "url": "/static/js/main.193652f9.chunk.js"
+  },
+  {
+    "revision": "cf548527d3449fe00c9e",
+    "url": "/static/js/runtime-main.5492b453.js"
+  }
+]);
